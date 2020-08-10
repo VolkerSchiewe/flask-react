3c41d6746d88e872f633971f9020ad9487640380
VolkerSchiewe <volker.s1994@gmail.com>
2020-08-10 10:42:43 +0200

    Add create-react-app, serve build with flask

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder='./build', template_folder="./build")
 
 
-@app.route("/")
-def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # TODO inject csrf token and cookies
+        return render_template('index.html', test="Hello World from flask")
 
 
 if __name__ == '__main__':
