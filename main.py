a67244a0da1194b668c04f374449054b7ac116af
VolkerSchiewe <volker.s1994@gmail.com>
2020-08-10 12:12:10 +0200

    Add csrf protection

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 
 from flask import Flask, render_template, send_from_directory
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__, static_folder='./build', template_folder="./build")
+app.secret_key = "bdTT0Y9zOEdZYNX4MycnL4nN8BFaY7P9"
+csrf = CSRFProtect(app)
 
 
 @app.route('/', defaults={'path': ''})
@@ -11,8 +14,7 @@ def serve(path):
     if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
-        # TODO inject csrf token and cookies
-        return render_template('index.html', test="Hello World from flask")
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
