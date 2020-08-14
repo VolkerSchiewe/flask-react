4883a2722b09bb496ce51e31f6c75f69e2d4a58b
VolkerSchiewe <volker.s1994@gmail.com>
2020-08-14 17:30:27 +0200

    Add example login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getCsrfToken} from "../utils/csrf";
+import {Button, Checkbox, FormControlLabel, TextField, Typography} from "@material-ui/core";
+import {useForm} from "react-hook-form";
 
 function App() {
-  console.log(getCsrfToken())
-  return (
-    <div className="App">
-      <header className="flex flex-col justify-center items-center h-screen text-white text-3xl bg-blue-900">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {register, handleSubmit, errors} = useForm()
+    function onSubmit(data: { }){
+        console.log(data)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col justify-center items-center h-screen bg-green-200">
+                <Typography variant={"h2"} className={"pb-6"}>Login</Typography>
+                <TextField type={"email"} helperText={errors["email"]?.message || " "} name={"email"} label={"Email"} variant={"outlined"} error={!!errors["email"]?.message} inputRef={register({required: "This field is required"})}/>
+                <TextField type={"password"} helperText={errors["password"]?.message || " "} name="password" label={"Password"} variant={"outlined"} error={!!errors["password"]?.message} inputRef={register({required: "This filed is required"})}/>
+                <FormControlLabel
+                    control={<Checkbox/>}
+                    name="remember_me"
+                    label="Remember me"
+                    inputRef={register}
+                />
+                <Button variant={"contained"} color={"primary"} type={"submit"}>Login</Button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
